f3c6639182eaa62c1264be6d20ea332f7bb29a1c
Sreerag N V <22mp1988@rit.ac.in>
2023-12-06 22:00:31 +0530

    Setting login page

---
+++ src/app/login/page.js
@@ -0,0 +1,42 @@
+
+export default function login() {
+    return (<div>
+        <div className="w-4/5 h-full relative bg-black rounded-3xl">
+    <div className="w-96 left-[49px] top-[198.74px] absolute text-white text-7xl font-bold font-['Anderson Grotesk']">Start your journey with us.</div>
+    <div className="w-7 h-7 left-[49px] top-[58.54px] absolute bg-orange-600 rounded-md" />
+    <div className="left-[83.67px] top-[57.92px] absolute text-white text-xl font-medium font-['HK Grotesk Wide']">bepatron.io</div>
+    <div className="w-2.5 h-2 left-[57.16px] top-[65.86px] absolute bg-white rounded-full" />
+    <div className="w-3.5 h-6 left-[57.93px] top-[88.27px] absolute">
+        <div className="left-0 top-0 absolute text-black text-xl font-normal font-['HK Grotesk Wide']">b</div>
+        <div className="w-2.5 h-2 left-[2.82px] top-[-10.86px] absolute bg-black rounded-full" />
+    </div>
+    <div className="left-[54.34px] top-[55px] absolute text-white text-xl font-normal font-['HK Grotesk Wide']">b</div>
+    <div className="w-2 h-2 left-[57.16px] top-[66.38px] absolute bg-white rounded-full" />
+    <div className="w-80 left-[49px] top-[396px] absolute text-zinc-400 text-xl font-normal font-['Anderson Grotesk'] leading-relaxed">Lorem ipsum dolor sit amet, <br/>consectetur adipiscing elit, sed do eius<br/>tempor incididunt ut labore et</div>
+    <div className="w-60 h-64 left-[513px] top-[615px] absolute">
+        <div className="w-60 h-64 left-0 top-0 absolute bg-zinc-900 rounded-3xl" />
+        <div className="w-48 left-[20px] top-[55px] absolute text-white text-sm font-normal font-['Anderson Grotesk'] leading-tight">Simply unbelievable! Lorem ipsum dolor sit amet, conse ctetur ad ipiscing  eiusmod tempor inci di dunt ut labore et dolore magna aliqua laboris nisi ut aliquip labo ris nisi ut aliquip</div>
+        <div className="left-[66px] top-[197px] absolute text-white text-base font-bold font-['Anderson Grotesk']">Hari Krishna</div>
+        <img className="w-14 h-8 left-[20px] top-[13px] absolute" src="https://via.placeholder.com/60x33" />
+        <div className="left-[66px] top-[215px] absolute text-white text-xs font-normal font-['Anderson Grotesk'] leading-tight">Products designer at google</div>
+        <img className="w-9 h-9 left-[20px] top-[197px] absolute rounded-full" src="https://via.placeholder.com/36x36" />
+    </div>
+    <div className="w-60 h-64 left-[-19px] top-[615px] absolute">
+        <div className="w-60 h-64 left-0 top-0 absolute bg-zinc-900 rounded-3xl" />
+        <div className="w-48 left-[20px] top-[55px] absolute text-white text-sm font-normal font-['Anderson Grotesk'] leading-tight">Simply unbelievable! Lorem ipsum dolor sit amet, conse ctetur ad ipiscing  eiusmod tempor inci di dunt ut labore et dolore magna aliqua laboris nisi ut aliquip labo ris nisi ut aliquip</div>
+        <div className="left-[66px] top-[197px] absolute text-white text-base font-bold font-['Anderson Grotesk']">Sreehari A J</div>
+        <img className="w-9 h-9 left-[20px] top-[197px] absolute rounded-full" src="https://via.placeholder.com/36x36" />
+        <div className="left-[66px] top-[215px] absolute text-white text-xs font-normal font-['Anderson Grotesk'] leading-tight">Senior manager at meta</div>
+        <img className="w-16 h-3 left-[20px] top-[23px] absolute" src="https://via.placeholder.com/64x13" />
+    </div>
+    <div className="w-60 h-64 left-[247px] top-[615px] absolute">
+        <div className="w-60 h-64 left-0 top-0 absolute bg-zinc-900 rounded-3xl" />
+        <div className="w-48 left-[20px] top-[55px] absolute text-white text-sm font-normal font-['Anderson Grotesk'] leading-tight">Simply unbelievable! Lorem ipsum dolor sit amet, conse ctetur ad ipiscing  eiusmod tempor inci di dunt ut labore et dolore magna aliqua laboris nisi ut aliquip labo ris nisi ut aliquip</div>
+        <div className="left-[66px] top-[197px] absolute text-white text-base font-bold font-['Anderson Grotesk']">Gopi Krishna</div>
+        <div className="left-[66px] top-[215px] absolute text-white text-xs font-normal font-['Anderson Grotesk'] leading-tight">SDE-2 at Amazon</div>
+        <img className="w-9 h-9 left-[19px] top-[197px] absolute rounded-full" src="https://via.placeholder.com/36x36" />
+        <img className="w-16 h-5 left-[18px] top-[19px] absolute" src="https://via.placeholder.com/63x22" />
+    </div>
+</div>
+    </div>)
+}
